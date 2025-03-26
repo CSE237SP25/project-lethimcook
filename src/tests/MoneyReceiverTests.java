1452a6d55ac8b0dd83bea79d2d1630f6f52c74e3
FunGuy-2 <lo.r@wustl.edu>
2025-03-26 13:42:29 -0500

    Updated with new feature for payment history

--- src/tests/MoneyReceiverTests.java
+++ src/tests/MoneyReceiverTests.java
@@ -12,7 +12,6 @@ import bankapp.MoneyReceiver;
 public class MoneyReceiverTests {
     @Test
     public void testSuccessfulMoneyReceive() {
-        // 1. Create objects to be tested
         Bank bank = new Bank();
         bank.createAccount("sender", "senderpass", "123456");
         bank.createAccount("recipient", "recipientpass", "789012");
@@ -24,69 +23,9 @@ public class MoneyReceiverTests {
         
         MoneyReceiver moneyReceiver = new MoneyReceiver(bank);
         
-        // 2. Call the method being tested
         moneyReceiver.receiveMoney(recipientAccount, "sender", 200.00);
         
-        // 3. Use assertions to verify results
         assertEquals(300.00, senderAccount.getBalance(), 0.005);
         assertEquals(300.00, recipientAccount.getBalance(), 0.005);
-    }
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void testInvalidSenderUsername() {
-        // 1. Create objects to be tested
-        Bank bank = new Bank();
-        bank.createAccount("recipient", "recipientpass", "789012");
-        
-        BankAccount recipientAccount = bank.getAccount("recipient");
-        MoneyReceiver moneyReceiver = new MoneyReceiver(bank);
-        
-        // 2. Call the method being tested
-        moneyReceiver.receiveMoney(recipientAccount, "nonexistent", 50.00);
-    }
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void testNegativeTransferAmount() {
-        // 1. Create objects to be tested
-        Bank bank = new Bank();
-        bank.createAccount("sender", "senderpass", "123456");
-        bank.createAccount("recipient", "recipientpass", "789012");
-        
-        BankAccount senderAccount = bank.getAccount("sender");
-        BankAccount recipientAccount = bank.getAccount("recipient");
-        
-        MoneyReceiver moneyReceiver = new MoneyReceiver(bank);
-        
-        // 2. Call the method being tested
-        moneyReceiver.receiveMoney(recipientAccount, "sender", -50.00);
-    }
-    
-    @Test(expected = IllegalStateException.class)
-    public void testInsufficientFundsSender() {
-        // 1. Create objects to be tested
-        Bank bank = new Bank();
-        bank.createAccount("sender", "senderpass", "123456");
-        bank.createAccount("recipient", "recipientpass", "789012");
-        
-        BankAccount senderAccount = bank.getAccount("sender");
-        BankAccount recipientAccount = bank.getAccount("recipient");
-        senderAccount.deposit(100.00);
-        
-        MoneyReceiver moneyReceiver = new MoneyReceiver(bank);
-        
-        // 2. Call the method being tested
-        moneyReceiver.receiveMoney(recipientAccount, "sender", 200.00);
-    }
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void testNullRecipientAccount() {
-        // 1. Create objects to be tested
-        Bank bank = new Bank();
-        bank.createAccount("sender", "senderpass", "123456");
-        
-        MoneyReceiver moneyReceiver = new MoneyReceiver(bank);
-        
-        // 2. Call the method being tested
-        moneyReceiver.receiveMoney(null, "sender", 50.00);
     }
 }
